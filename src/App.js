198da78e2d92e8c893d1a2fba2723d7b5a4e796e
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-09-14 15:03:16 -0300

    course info, step 6

--- src/App.js
+++ src/App.js
@@ -16,9 +16,12 @@ const Header = ({ course }) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
-      <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
-      <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
+      {course.parts.map((part, i) => (
+        <Part
+          part={course.parts[i].name}
+          exercises={course.parts[i].exercises}
+        />
+      ))}
     </div>
   )
 }
